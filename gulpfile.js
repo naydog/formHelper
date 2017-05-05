d656f7ae151337b3348dd7b779fd8246a9536e53
Henry Yan <yanhwh@cn.ibm.com>
2017-05-05 17:52:14 +0800

    minify css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp"),
   uglify = require("gulp-uglify"),
-  rename = require("gulp-rename");
+  rename = require("gulp-rename"),
+  cleancss = require('gulp-clean-css');
 
 gulp.task('minifyjs', function() {
   var options = {
@@ -10,8 +11,15 @@ gulp.task('minifyjs', function() {
     .pipe(uglify(options))
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('dest/'));
-})
+});
+
+gulp.task('cleancss', function() {
+  gulp.src(['src/*.css'])
+    .pipe(cleancss())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dest/'));
+});
 
 gulp.task("default", function() {
-  gulp.start('minifyjs');
+  gulp.start('minifyjs', 'cleancss');
 });
